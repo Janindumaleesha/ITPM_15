15685ad84a1832b76712ed4a0167443757b3bde8
YasasviKaru <yasasvikarunarathne@icloud.com>
2022-04-04 00:55:07 +0530

    Add item

--- src/controllers/item.controllers.js
+++ src/controllers/item.controllers.js
@@ -1,1 +1,24 @@
-console.log("Hello, my first line.")
+//import item modals
+
+const itemModal = require('../modals/item.modals')
+
+//add item modals
+
+additem = async(req,res) => {
+    try{
+        const createItem = await itemModal.create(req.body)
+        if(createItem){
+            res.status(201).send("Item created")
+        }
+        else{
+            res.status(204).send("Invalid Item creation")
+        }
+    }
+    catch(err){
+        res.status(500).send("Something went wrong")
+    }
+}
+
+module.exports = {
+    additem
+}
