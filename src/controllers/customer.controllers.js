244e0a3031367526ba574b2c54a5889f83694ecc
Acer <gethminisansa@gmail.com>
2022-04-03 23:49:14 +0530

    Add customer

--- src/controllers/customer.controllers.js
+++ src/controllers/customer.controllers.js
@@ -1,1 +1,25 @@
-console.log("Gethmi")
+//import customer modal
+const customermodal = require('../modals/customer.modals')
+
+//Add customer
+addcustomer = async(req,res) => {
+
+    try{
+        const createCustomer = await customermodal.create(req.body)
+
+        if(createCustomer){
+            res.status(201).send("Customer created")
+        }
+        else{
+            res.status(204).send("Invalid Customer creation")
+        }
+
+    }
+    catch(err){
+        res.status(500).send("Something went wrong")
+    }
+}
+
+module.exports = {
+    addcustomer
+}
