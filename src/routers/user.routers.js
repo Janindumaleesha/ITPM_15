7590bd5281e4171bc2601ff69520288a72abc39b
Acer <gethminisansa@gmail.com>
2022-04-14 00:58:54 +0530

    Create user Sign up

--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -2,6 +2,9 @@ const express = require('express')
 const UserController = require('../controllers/user.controllers')
 const UserRouter = express.Router()
 
+//USER REGISTER 
+UserRouter.post('/register' , UserController.userRegister)
+
 //USER LOGIN
 UserRouter.post('/login' , UserController.userLogin)
 
