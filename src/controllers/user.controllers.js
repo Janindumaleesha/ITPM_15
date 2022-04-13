d943f2ea77e0a975a650a9eb3bc79d229ffd80d8
YasasviKaru <yasasvikarunarathne@icloud.com>
2022-04-14 00:40:46 +0530

    Create User Sign In

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,37 +1,5 @@
-//import user modals
-const usermodals = require('../modals/user.modals')
 //import auth modals
 const authmodals = require('../modals/auth.modals')
-
-//user register
-userRegister=async( req , res )=>{
-
-    try{
-        const alreadyUser = await usermodals.findOne({email:req.body.email})
-        if(!alreadyUser){
-            const userRegister = await usermodals.create(req.body)
-            const authUser = await authmodals.create({userId:userRegister._id,password:req.body.password})
-            if(userRegister && authUser){
-                res.status(201).send(userRegister)
-            }
-            else{
-                res.status(404).send('Something went wrong')
-            }
-            // UserModal.create(req.body).then((user)=>{
-            //     res.send(user)
-            // }).catch((err)=>{
-            //     res.status(201).send(err)
-            // })
-        }
-        else{
-            res.status(401).send('This user is already existed')
-        }
-    }
-    catch(err){
-        req.status(500).send(err)
-    }
-
-}
 
 //user login
 userLogin=async( req , res )=>{
@@ -62,6 +30,5 @@ userLogin=async( req , res )=>{
 }
 
 module.exports = {
-    userRegister,
     userLogin
 }
