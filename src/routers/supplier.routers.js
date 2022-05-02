a77b84664b5c9d76d669cee2c6f1156d959b82e1
NaliniSandunika <sandunikahindurangala92@gmail.com>
2022-05-02 22:46:44 +0530

    Create supplier routers update and delete

--- src/routers/supplier.routers.js
+++ src/routers/supplier.routers.js
@@ -11,4 +11,10 @@ supplierRouter.get('/getsupplier' , supplierController.getsuppliers)
 //Get Supplier By Id
 supplierRouter.get('/getsupplierid/:supplierId' , supplierController.getsupplierId)
 
+//Update Supplier
+supplierRouter.put('/updatesupplier/:supplierId' , supplierController.updatesupplier)
+
+//Delete Supplier
+supplierRouter.delete('/deletesupplier/:supplierId' , supplierController.deletesupplier)
+
 module.exports = supplierRouter
